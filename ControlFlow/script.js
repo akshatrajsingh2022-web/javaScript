72afb0b99259d5812e86c71291c704ed58defad4
Akshat raj singh <akshatrajsingh2022@gmail.com>
2026-06-26 11:57:09 +0530

    script.js

---
+++ ControlFlow/script.js
@@ -0,0 +1,43 @@
+// if else elseif
+if(loggedin && admin){
+
+}
+else if(loggedin){
+
+}
+else{
+
+}
+
+
+
+// Switch case
+switch(val){
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    default:
+        console.log("Enter valid value");
+        break;
+
+}
+ 
+
+
+
+
+// Early return pattern
+function getval(val){
+    if(val<25) return "D";
+    else if(val<50) return "C";
+    else if(val<75) return "B";
+    else return "A";
+}
+console.log(getval(35));
+
+
+
+   //ROCK-PAPER-SCISSORS LOGIC
