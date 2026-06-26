8d93db4d8c7616d87b9ea18290b5ed156ce7aaba
Akshat raj singh <akshatrajsingh2022@gmail.com>
2026-06-26 18:07:25 +0530

    script

---
+++ loops/script.js
@@ -0,0 +1,30 @@
+/*
+ //FOR LOOP
+for(let i=1;i<=100;i++){
+    console.log(i);
+}
+
+
+
+//WHILE LOOP
+let a=1;
+while(a<=10){
+    console.log(a);
+    a++;
+}
+
+//DO WHILE
+let i=0;
+do{
+    console.log("Yeah its a positive no.");
+    i++;
+}
+while(i>0)
+
+*/
+//break and continue
+for(let i=0;i<20;i++){
+    if (i==5) continue;
+    else if (i==12) break;
+    else console.log(i);
+}
